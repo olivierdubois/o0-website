b15f813c48539060e37677631f25caf0226137ad
Olivier Dubois <olivier@olivierdubois.com>
2014-01-05 17:58:32 -0800

    Update 'page.tpl.php' theme template file in 'o0' theme

--- sites/all/themes/o0/templates/page.tpl.php
+++ sites/all/themes/o0/templates/page.tpl.php
@@ -107,14 +107,18 @@
           <?php endif; ?>
           <?php print render($title_prefix); ?>
           <?php if ($title): ?>
-
             <div id="content-title"><h1 class="title" id="page-title"><?php print $title; ?></h1></div>
           <?php endif; ?>
           <?php print render($title_suffix); ?>
           <?php if ($tabs): ?>
             <?php print render($tabs); ?>
           <?php endif; ?>
-          <?php print render($page['help']); ?>
+          <?php if ($messages): ?>
+            <div id="message"><?php print $messages; ?></div>
+          <?php endif; ?>
+          <?php if ($page['help']): ?>
+            <div id="help"><?php print render($page['help']); ?></div>
+          <?php endif; ?>
           <?php if ($action_links): ?>
             <ul class="action-links">
               <?php print render($action_links); ?>
