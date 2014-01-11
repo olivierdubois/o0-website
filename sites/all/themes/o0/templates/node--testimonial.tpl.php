25204dca6c3942131406cfcabcd26ffaba097455
Olivier Dubois <olivier@olivierdubois.com>
2014-01-10 23:28:33 -0800

    Add 'Testimonial' content type
    Initial implementation of the 'Testimonial' content type and its related features.
    
    - Added 'node--testimonial.tpl.php' template file in 'o0' theme.
    
    - Added '_content-testimonial.scss' Sass file in 'o0' theme.

---
+++ sites/all/themes/o0/templates/node--testimonial.tpl.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @file
+ * 'Testimonial' content type.
+ * 'Testimonial' node template.
+ *
+ */
+?>
+<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <header>
+    <?php print render($title_prefix); ?>
+    <?php if (!$page): ?>
+      <h2<?php print $title_attributes; ?> class="node-title title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
+
+    <?php if ($display_submitted): ?>
+      <div class="meta submitted">
+        <?php //print $user_picture; ?>
+        <?php //print $submitted; ?>
+      </div>
+    <?php endif; ?>
+  </header>
+  
+  <div class="node-content clearfix"<?php print $content_attributes; ?>>
+    <?php
+      // Hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+    ?>
+
+    <?php if (!empty($content['body'])): ?>
+      <?php print render($content['body']); ?>
+    <?php endif; ?>
+    
+  </div>
+
+  <footer>
+    <?php
+      // Remove the "Add new comment" link on the teaser page or if the comment
+      // form is being displayed on the same page.
+      if ($teaser || !empty($content['comments']['comment_form'])) {
+        unset($content['links']['comment']['#links']['comment-add']);
+      }
+      // Only display the wrapper div if there are links.
+      $links = render($content['links']);
+    ?>
+    <?php if ($links): ?>
+      <div class="link-wrapper">
+        <?php print $links; ?>
+      </div>
+    <?php endif; ?>
+  </footer>
+
+  <section>
+    <?php print render($content['comments']); ?>
+  </section>
+
+</article>
