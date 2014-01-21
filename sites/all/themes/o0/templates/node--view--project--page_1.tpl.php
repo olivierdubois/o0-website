8410f36d153da47911b45f930fe37daf99e6e8ac
Olivier Dubois <olivier@olivierdubois.com>
2014-01-20 23:55:04 -0800

    Update 'Project' template file for views in grid format and related CSS in 'o0' theme

--- sites/all/themes/o0/templates/node--view--project--page_1.tpl.php
+++ sites/all/themes/o0/templates/node--view--project--page_1.tpl.php
@@ -8,36 +8,38 @@
 ?>
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <div class="node-content clearfix"<?php print $content_attributes; ?>>
-    <?php
-      // Hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-    ?>
+  <a class="blocklevel" href="<?php print $node_url; ?>">
+    <div class="node-content clearfix"<?php print $content_attributes; ?>>
+      <?php
+        // Hide the comments and links now so that we can render them later.
+        hide($content['comments']);
+        hide($content['links']);
+      ?>
 
-    <?php if (!empty($content['field_project_image_t'])): ?>
-      <div class="field field-name-field-project-image field-type-image"><div class="field-items"><div class="field-item"><a href="<?php print $node_url; ?>"><img typeof="foaf:Image" src="<?php print image_style_url('project_image_view_page_grid', $node->field_project_image_t['und'][0]['uri']); ?>" alt="" /></a></div></div></div>
-    <?php elseif (!empty($content['field_project_image'])): ?>
-      <div class="field field-name-field-project-image field-type-image"><div class="field-items"><div class="field-item"><a href="<?php print $node_url; ?>"><img typeof="foaf:Image" src="<?php print image_style_url('project_image_view_page_grid', $node->field_project_image['und'][0]['uri']); ?>" alt="" /></a></div></div></div>
-    <?php else: ?>
-      <div class="field field-name-field-project-image field-type-image"><div class="field-items"><div class="field-item"><a href="<?php print $node_url; ?>"><img typeof="foaf:Image" src="<?php print image_style_url('project_image_view_page_grid', $content_placeholder_image); ?>" alt="" class="<?php print $content_placeholder_image_classes; ?>" /></a></div></div></div>
-    <?php endif; ?>
-
-    <?php print render($title_prefix); ?>
-    <h2<?php print $title_attributes; ?> class="node-title title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-    <?php print render($title_suffix); ?>
-
-    <?php if (!empty($content['field_project_type'])): ?>
-      <?php print render($content['field_project_type']); ?>
-    <?php endif; ?>
-
-    <div class="meta">
-      <?php if (!empty($content['field_project_date'])): ?>
-        <?php print render($content['field_project_date']); ?>
+      <?php if (!empty($content['field_project_image_t'])): ?>
+        <div class="field field-name-field-project-image field-type-image"><div class="field-items"><div class="field-item"><img typeof="foaf:Image" src="<?php print image_style_url('project_image_view_page_grid', $node->field_project_image_t['und'][0]['uri']); ?>" alt="" /></div></div></div>
+      <?php elseif (!empty($content['field_project_image'])): ?>
+        <div class="field field-name-field-project-image field-type-image"><div class="field-items"><div class="field-item"><img typeof="foaf:Image" src="<?php print image_style_url('project_image_view_page_grid', $node->field_project_image['und'][0]['uri']); ?>" alt="" /></div></div></div>
+      <?php else: ?>
+        <div class="field field-name-field-project-image field-type-image"><div class="field-items"><div class="field-item"><img typeof="foaf:Image" src="<?php print image_style_url('project_image_view_page_grid', $content_placeholder_image); ?>" alt="" class="<?php print $content_placeholder_image_classes; ?>" /></div></div></div>
       <?php endif; ?>
-    </div>
 
-  </div>
+      <?php print render($title_prefix); ?>
+      <h2<?php print $title_attributes; ?> class="node-title title"><?php print $title; ?></h2>
+      <?php print render($title_suffix); ?>
+
+      <?php if (!empty($content['field_project_type'])): ?>
+        <?php print render($content['field_project_type']); ?>
+      <?php endif; ?>
+
+      <div class="meta">
+        <?php if (!empty($content['field_project_date'])): ?>
+          <?php print render($content['field_project_date']); ?>
+        <?php endif; ?>
+      </div>
+
+    </div>
+  </a>
 
   <footer>
     <?php
