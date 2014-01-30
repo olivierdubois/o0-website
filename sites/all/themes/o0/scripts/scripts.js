f82a2dbe1808ce268b77974f6dfec29384c492dc
Olivier Dubois <olivier@olivierdubois.com>
2014-01-29 22:25:11 -0800

    Update CSS for 'Organization' content type in 'o0' theme
    - Updated '_content-organization.scss' Sass file.
    
    - Added JavaScript function to Unify width and height of 'field-item' container in grid view.

--- sites/all/themes/o0/scripts/scripts.js
+++ sites/all/themes/o0/scripts/scripts.js
@@ -5,8 +5,38 @@ $ = jQuery;
 
 
 /**
- * jQuery Knob
+ * jQuery Knob.
  */
 $(function() {
   $(".chart-doughnut").knob();
+});
+
+
+
+
+/**
+ * Unify width and height of 'field-item' container in grid view.
+ * This can be used to align images of different sizes (such as logos, etc.) on
+ * the center of the 'field-item' containers.
+ */
+$(function() {
+  function unifyContainer() {
+    var widthMax = 0;
+    var heightMax = 0;
+    $('.field-items').children('.field-item').each(function() {
+      var height = $(this).outerHeight();
+      // alert(height);
+      if ( height > heightMax ) {
+        heightMax = height;
+      }
+      var width = $(this).outerWidth();
+      // alert(width);
+      if ( width > widthMax ) {
+      widthMax = width;
+      }
+    });
+    $('.field-item').css('height', heightMax);
+    $('.field-item').css('width', widthMax);
+  }
+  unifyContainer();
 });
