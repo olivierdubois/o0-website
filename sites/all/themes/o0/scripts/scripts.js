06d7ca2cadd12dea8914414f4c81b5b1a8560f16
Olivier Dubois <olivier@olivierdubois.com>
2014-01-30 23:07:35 -0800

    Update 'unifyContainer' JavaScript function in 'o0' theme

--- sites/all/themes/o0/scripts/scripts.js
+++ sites/all/themes/o0/scripts/scripts.js
@@ -19,24 +19,21 @@ $(function() {
  * This can be used to align images of different sizes (such as logos, etc.) on
  * the center of the 'field-item' containers.
  */
-$(function() {
-  function unifyContainer() {
-    var widthMax = 0;
-    var heightMax = 0;
-    $('.field-items').children('.field-item').each(function() {
-      var height = $(this).outerHeight();
-      // alert(height);
-      if ( height > heightMax ) {
-        heightMax = height;
-      }
-      var width = $(this).outerWidth();
-      // alert(width);
-      if ( width > widthMax ) {
-      widthMax = width;
-      }
-    });
-    $('.field-item').css('height', heightMax);
-    $('.field-item').css('width', widthMax);
-  }
-  unifyContainer();
-});
+function unifyContainer() {
+  var widthMax = 0;
+  var heightMax = 0;
+  $('.field-items').children('.field-item').each(function() {
+    var height = $(this).outerHeight();
+    // alert(height);
+    if ( height > heightMax ) {
+      heightMax = height;
+    }
+    var width = $(this).outerWidth();
+    // alert(width);
+    if ( width > widthMax ) {
+    widthMax = width;
+    }
+  });
+  $('.field-item').css('height', heightMax);
+  $('.field-item').css('width', widthMax);
+}
