09c46fbd9cecf991b05d9d8d8fe04004ac04d07b
Olivier Dubois <olivier@olivierdubois.com>
2014-02-02 11:30:54 -0800

    Update 'Article' template files with block-level link in 'o0' theme
    - Added block-level link in 'node--view--blog--page_1.tpl.php' view template file.
    
    - Added block-level link in 'node--view--microblog--page_1.tpl.php' view template file.
    
    - Updated related Sass files with CSS rules.

--- sites/all/themes/o0/templates/node--view--microblog--page_1.tpl.php
+++ sites/all/themes/o0/templates/node--view--microblog--page_1.tpl.php
@@ -8,31 +8,33 @@
 ?>
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <div class="node-content clearfix"<?php print $content_attributes; ?>>
-    <?php
-      // Hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-    ?>
+  <a class="blocklevel" href="<?php print $node_url; ?>">
+    <div class="node-content clearfix"<?php print $content_attributes; ?>>
+      <?php
+        // Hide the comments and links now so that we can render them later.
+        hide($content['comments']);
+        hide($content['links']);
+      ?>
 
-    <?php if (!empty($content['field_article_image'])): ?>
-      <?php //print render($content['field_article_image']); ?>
-      <div class="field field-name-field-article-image field-type-image"><div class="field-items"><div class="field-item"><a href="<?php print $node_url; ?>"><img typeof="foaf:Image" src="<?php print image_style_url('article_image_view_page_grid', $node->field_article_image['und'][0]['uri']); ?>" alt="" /></a></div></div></div>
-    <?php else: ?>
-      <div class="field field-name-field-article-image field-type-image"><div class="field-items"><div class="field-item"><a href="<?php print $node_url; ?>"><img typeof="foaf:Image" src="<?php print image_style_url('article_image_view_page_grid', $content_placeholder_image); ?>" alt="" class="<?php print $content_placeholder_image_classes; ?>" /></a></div></div></div>
-    <?php endif; ?>
-
-    <?php print render($title_prefix); ?>
-    <h2<?php print $title_attributes; ?> class="node-title title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-    <?php print render($title_suffix); ?>
-
-    <div class="meta">
-      <?php if (!empty($content['field_global_pub_date'])): ?>
-        <?php print render($content['field_global_pub_date']); ?>
+      <?php if (!empty($content['field_article_image'])): ?>
+        <?php //print render($content['field_article_image']); ?>
+        <div class="field field-name-field-article-image field-type-image"><div class="field-items"><div class="field-item"><img typeof="foaf:Image" src="<?php print image_style_url('article_image_view_page_grid', $node->field_article_image['und'][0]['uri']); ?>" alt="" /></div></div></div>
+      <?php else: ?>
+        <div class="field field-name-field-article-image field-type-image"><div class="field-items"><div class="field-item"><img typeof="foaf:Image" src="<?php print image_style_url('article_image_view_page_grid', $content_placeholder_image); ?>" alt="" class="<?php print $content_placeholder_image_classes; ?>" /></div></div></div>
       <?php endif; ?>
-    </div>
 
-  </div>
+      <?php print render($title_prefix); ?>
+      <h2<?php print $title_attributes; ?> class="node-title title"><?php print $title; ?></h2>
+      <?php print render($title_suffix); ?>
+
+      <div class="meta">
+        <?php if (!empty($content['field_global_pub_date'])): ?>
+          <?php print render($content['field_global_pub_date']); ?>
+        <?php endif; ?>
+      </div>
+
+    </div>
+  </a>
 
   <footer>
     <?php
