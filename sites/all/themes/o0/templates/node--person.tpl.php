b70383066750e950bc8e8a34470b68338c56e026
Olivier Dubois <olivier@olivierdubois.com>
2014-02-27 23:22:51 -0800

    Update 'Person' template file in 'o0' theme
    - Added amCharts JavaScript to create and render charts for the 'skill' fields.

--- sites/all/themes/o0/templates/node--person.tpl.php
+++ sites/all/themes/o0/templates/node--person.tpl.php
@@ -112,36 +112,110 @@
     <?php if (!empty($content['field_person_skill'])): ?>
       <h4>Web technologies</h4>
       <div class="field field-name-field-person-skill field-type-field-collection clearfix">
-        <div class="field-items">
-          <?php foreach($content['field_person_skill']['#items'] as $entity_uri) :
-            $field_collection_item = entity_load('field_collection_item', $entity_uri);
-            foreach($field_collection_item as $field_collection_object) : ?>
-              <div class="field-item">
-                  <?php if ($field_collection_object->field_person_skill_skill && $field_collection_object->field_person_skill_level): ?>
-                    <div class="field field-name-field-person-skill-level field-type-text"><input class="chart-doughnut" value="<?php print $field_collection_object->field_person_skill_level['und'][0]['value']; ?>" data-width="120" data-height="120" data-angleOffset=-125 data-angleArc=250 data-readOnly=true /></div>
-                    <div class="field field-name-field-person-skill-skill field-type-text"><?php print $field_collection_object->field_person_skill_skill['und'][0]['value']; ?></div>
-                  <?php endif; ?>
-              </div>
+        <script type="text/javascript">
+          // Build the array containing the data for the chart.
+          var chart_person_skills_1__chartData = [
+            <?php foreach($content['field_person_skill']['#items'] as $entity_uri) :
+              $field_collection_item = entity_load('field_collection_item', $entity_uri);
+              foreach($field_collection_item as $field_collection_object) : ?>
+                <?php if ($field_collection_object->field_person_skill_skill && $field_collection_object->field_person_skill_level): ?>
+                  {
+                    "skill": "<?php print $field_collection_object->field_person_skill_skill['und'][0]['value']; ?>",
+                    "level": <?php print $field_collection_object->field_person_skill_level['und'][0]['value']; ?>
+                  },
+                <?php endif; ?>
             <?php endforeach; endforeach; ?>
-        </div>
+          ];
+
+          // Build the chart.
+          AmCharts.ready(function() {
+            var chart_person_skills_1__chart = new AmCharts.AmSerialChart();
+            chart_person_skills_1__chart.dataProvider = chart_person_skills_1__chartData;
+            chart_person_skills_1__chart.categoryField = "skill";
+            chart_person_skills_1__chart.startDuration = 1;
+            chart_person_skills_1__chart.creditsPosition = "top-right";
+
+            var chart_person_skills_1__graph = new AmCharts.AmGraph();
+            chart_person_skills_1__graph.valueField = "level";
+            chart_person_skills_1__graph.type = "column";
+            chart_person_skills_1__graph.lineAlpha = 0;
+            chart_person_skills_1__graph.fillAlphas = 0.8;
+            chart_person_skills_1__graph.balloonText = "[[category]]: <b>[[value]]</b>";
+            chart_person_skills_1__chart.addGraph(chart_person_skills_1__graph);
+
+            var chart_person_skills_1__categoryAxis = chart_person_skills_1__chart.categoryAxis;
+            chart_person_skills_1__categoryAxis.autoGridCount  = false;
+            chart_person_skills_1__categoryAxis.gridCount = chart_person_skills_1__chartData.length;
+            chart_person_skills_1__categoryAxis.gridPosition = "start";
+            chart_person_skills_1__categoryAxis.autoWrap = true;
+            chart_person_skills_1__categoryAxis.labelRotation = 0;
+
+            var chart_person_skills_1__valueAxis = new AmCharts.ValueAxis();
+            chart_person_skills_1__valueAxis.minimum = 0;
+            chart_person_skills_1__valueAxis.maximum = 100;
+            chart_person_skills_1__valueAxis.unit = "%";
+            chart_person_skills_1__valueAxis.autoGridCount = false;
+            chart_person_skills_1__chart.addValueAxis(chart_person_skills_1__valueAxis);
+
+            chart_person_skills_1__chart.write("chart-person-skills-1");
+          });
+        </script>
+        <div id="chart-person-skills-1" class="chart chart-person-skills amcharts"></div>
       </div>
     <?php endif; ?>
 
     <?php if (!empty($content['field_person_skill_2'])): ?>
       <h4>Content management systems, etc.</h4>
-      <div class="field field-name-field-person-skill-2 field-type-field-collection clearfix">
-        <div class="field-items">
-          <?php foreach($content['field_person_skill_2']['#items'] as $entity_uri) :
-            $field_collection_item = entity_load('field_collection_item', $entity_uri);
-            foreach($field_collection_item as $field_collection_object) : ?>
-              <div class="field-item">
+      <div class="field field-name-field-person-skill field-type-field-collection clearfix">
+        <script type="text/javascript">
+          // Build the array containing the data for the chart.
+          var chart_person_skills_2__chartData = [
+            <?php foreach($content['field_person_skill_2']['#items'] as $entity_uri) :
+              $field_collection_item = entity_load('field_collection_item', $entity_uri);
+              foreach($field_collection_item as $field_collection_object) : ?>
                 <?php if ($field_collection_object->field_person_skill_skill && $field_collection_object->field_person_skill_level): ?>
-                  <div class="field field-name-field-person-skill-level field-type-text"><input class="chart-doughnut" value="<?php print $field_collection_object->field_person_skill_level['und'][0]['value']; ?>" data-width="120" data-height="120" data-angleOffset=-125 data-angleArc=250 data-readOnly=true /></div>
-                  <div class="field field-name-field-person-skill-skill field-type-text"><?php print $field_collection_object->field_person_skill_skill['und'][0]['value']; ?></div>
+                  {
+                    "skill": "<?php print $field_collection_object->field_person_skill_skill['und'][0]['value']; ?>",
+                    "level": <?php print $field_collection_object->field_person_skill_level['und'][0]['value']; ?>
+                  },
                 <?php endif; ?>
-              </div>
             <?php endforeach; endforeach; ?>
-        </div>
+          ];
+
+          // Build the chart.
+          AmCharts.ready(function() {
+            var chart_person_skills_2__chart = new AmCharts.AmSerialChart();
+            chart_person_skills_2__chart.dataProvider = chart_person_skills_2__chartData;
+            chart_person_skills_2__chart.categoryField = "skill";
+            chart_person_skills_2__chart.startDuration = 1;
+            chart_person_skills_2__chart.creditsPosition = "top-right";
+
+            var chart_person_skills_2__graph = new AmCharts.AmGraph();
+            chart_person_skills_2__graph.valueField = "level";
+            chart_person_skills_2__graph.type = "column";
+            chart_person_skills_2__graph.lineAlpha = 0;
+            chart_person_skills_2__graph.fillAlphas = 0.8;
+            chart_person_skills_2__graph.balloonText = "[[category]]: <b>[[value]]</b>";
+            chart_person_skills_2__chart.addGraph(chart_person_skills_2__graph);
+
+            var chart_person_skills_2__categoryAxis = chart_person_skills_2__chart.categoryAxis;
+            chart_person_skills_2__categoryAxis.autoGridCount  = false;
+            chart_person_skills_2__categoryAxis.gridCount = chart_person_skills_2__chartData.length;
+            chart_person_skills_2__categoryAxis.gridPosition = "start";
+            chart_person_skills_2__categoryAxis.autoWrap = true;
+            chart_person_skills_2__categoryAxis.labelRotation = 0;
+
+            var chart_person_skills_2__valueAxis = new AmCharts.ValueAxis();
+            chart_person_skills_2__valueAxis.minimum = 0;
+            chart_person_skills_2__valueAxis.maximum = 100;
+            chart_person_skills_2__valueAxis.unit = "%";
+            chart_person_skills_2__valueAxis.autoGridCount = false;
+            chart_person_skills_2__chart.addValueAxis(chart_person_skills_2__valueAxis);
+
+            chart_person_skills_2__chart.write("chart-person-skills-2");
+          });
+        </script>
+        <div id="chart-person-skills-2" class="chart chart-person-skills amcharts"></div>
       </div>
     <?php endif; ?>
 
