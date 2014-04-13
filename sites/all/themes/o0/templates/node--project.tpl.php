9d904abe1d6261d6ab5e1bb79b8eb10b4e38cad2
Olivier Dubois <olivier@olivierdubois.com>
2014-04-12 20:54:45 -0700

    Update 'node--project.tpl.php' template file in 'o0' theme
    - Renamed 'field_global_r_organization' entity reference field to 'field_project_r_organization'.
    
    - Renamed 'field_global_t_person_exper__term' term reference field to 'field_project_t_person_exper__term'.
    
    - Added image caption to colorbox links.

--- sites/all/themes/o0/templates/node--project.tpl.php
+++ sites/all/themes/o0/templates/node--project.tpl.php
@@ -47,21 +47,20 @@
 
         <?php if (!empty($content['field_project_image'])): ?>
           <?php if (empty($content['field_project_image_mobile'])): ?>
-            <div class="field field-name-field-project-image field-type-image show-for-small"><div class="field-items"><div class="field-item"><a href="<?php print image_style_url($variables['field_project_image__image_style__fullscreen'], $node->field_project_image['und'][0]['uri']); ?>" class="colorbox-inline" rel="node-field-project-image"><img typeof="foaf:Image" src="<?php print image_style_url($variables['field_project_image__image_style__node'], $node->field_project_image['und'][0]['uri']); ?>" alt="<?php print $node->field_project_image['und'][0]['alt']; ?>" title="<?php print $node->field_project_image['und'][0]['title']; ?>" /></a></div></div></div>
+            <div class="field field-name-field-project-image field-type-image show-for-small"><div class="field-items"><div class="field-item"><a href="<?php print image_style_url($variables['field_project_image__image_style__fullscreen'], $node->field_project_image['und'][0]['uri']); ?>" class="colorbox" rel="node-field-project-image" title="<?php print $node->field_project_image['und'][0]['title']; ?>"><img typeof="foaf:Image" src="<?php print image_style_url($variables['field_project_image__image_style__node'], $node->field_project_image['und'][0]['uri']); ?>" alt="<?php print $node->field_project_image['und'][0]['alt']; ?>" title="<?php print $node->field_project_image['und'][0]['title']; ?>" /></a></div></div></div>
           <?php else: ?>
             <div class="field field-name-field-project-image field-type-image show-for-small"><div class="field-items"><div class="field-item">
-              <a href="<?php print image_style_url($variables['field_project_image__image_style__fullscreen'], $node->field_project_image['und'][0]['uri']); ?>" class="colorbox-inline" rel="node-field-project-image">
+              <a href="<?php print image_style_url($variables['field_project_image__image_style__fullscreen'], $node->field_project_image['und'][0]['uri']); ?>" class="colorbox" rel="node-field-project-image">
                 <img typeof="foaf:Image" src="<?php print image_style_url($variables['field_project_image__image_style__node'], $node->field_project_image['und'][0]['uri']); ?>" alt="" />
                 <img class="field-name-field-project-image-mobile-laptop-overlay" typeof="foaf:Image" src="<?php print image_style_url('project_image_mobile_laptop_overlay', $node->field_project_image_mobile['und'][0]['uri']); ?>" alt="<?php print $node->field_project_image_mobile['und'][0]['alt']; ?>" title="<?php print $node->field_project_image_mobile['und'][0]['title']; ?>" />
               </a>
-              <a href="<?php print image_style_url('project_image_mobile_modal_fullscreen', $node->field_project_image_mobile['und'][0]['uri']); ?>" class="colorbox-inline" rel="node-field-project-image"></a>
             </div></div></div>
           <?php endif; ?>
         <?php else: ?>
           <div class="field field-name-field-project-image field-type-image show-for-small"><div class="field-items"><div class="field-item"><img typeof="foaf:Image" src="<?php print image_style_url($variables['field_project_image__image_style__node'], $variables['content_placeholder_image']); ?>" alt="Placeholder image" class="<?php print $variables['content_placeholder_image_classes']; ?>" /></div></div></div>
         <?php endif; ?>
 
-        <?php if (!empty($content['field_global_r_organization'])): ?>
+        <?php if (!empty($content['field_project_r_organization'])): ?>
           <?php print views_embed_view('organization_reference', 'project_node_body', $node->nid); ?>
         <?php endif; ?>
 
@@ -69,29 +68,29 @@
           <?php print render($content['body']); ?>
         <?php endif; ?>
 
-        <?php if (!empty($content['field_global_t_service'])): ?>
+        <?php if (!empty($content['field_project_t_service'])): ?>
           <h4>My work on this project</h4>
-          <?php print render($content['field_global_t_service']); ?>
+          <?php print render($content['field_project_t_service']); ?>
         <?php endif; ?>
 
-        <?php if (!empty($variables['field_global_t_person_exper__term'])): ?>
-          <?php if ($variables['field_global_t_person_exper__term'] == 'Freelance web developer'): ?>
-            <div class="field field-name-field-global-t-person-exper-descr field-type-text"><div class="field-items"><div class="field-item">
+        <?php if (!empty($variables['field_project_t_person_exper__term'])): ?>
+          <?php if ($variables['field_project_t_person_exper__term'] == 'Freelance web developer'): ?>
+            <div class="field field-name-field-project-t-person-exper-descr field-type-text"><div class="field-items"><div class="field-item">
               <p>This project was completed as part of my freelance web development work.</p>
             </div></div></div>
           <?php endif; ?>
-          <?php if ($variables['field_global_t_person_exper__term'] == 'Shervin'): ?>
-            <div class="field field-name-field-global-t-person-exper-descr field-type-text"><div class="field-items"><div class="field-item">
+          <?php if ($variables['field_project_t_person_exper__term'] == 'Shervin'): ?>
+            <div class="field field-name-field-project-t-person-exper-descr field-type-text"><div class="field-items"><div class="field-item">
               <p>This project was completed as part of my work as Lead Web Developer at Shervin Communications.</p>
             </div></div></div>
           <?php endif; ?>
-          <?php if ($variables['field_global_t_person_exper__term'] == 'Wild ARC'): ?>
-            <div class="field field-name-field-global-t-person-exper-descr field-type-text"><div class="field-items"><div class="field-item">
+          <?php if ($variables['field_project_t_person_exper__term'] == 'Wild ARC'): ?>
+            <div class="field field-name-field-project-t-person-exper-descr field-type-text"><div class="field-items"><div class="field-item">
               <p>This project was completed as part of my volunteer work at the BC SPCA Wild ARC.</p>
             </div></div></div>
           <?php endif; ?>
-          <?php if ($variables['field_global_t_person_exper__term'] == 'Freelance graphic designer'): ?>
-            <div class="field field-name-field-global-t-person-exper-descr field-type-text"><div class="field-items"><div class="field-item">
+          <?php if ($variables['field_project_t_person_exper__term'] == 'Freelance graphic designer'): ?>
+            <div class="field field-name-field-project-t-person-exper-descr field-type-text"><div class="field-items"><div class="field-item">
                   <p>This project was completed as part of my freelance graphic design work.</p>
                 </div></div></div>
           <?php endif; ?>
@@ -107,14 +106,13 @@
 
         <?php if (!empty($content['field_project_image'])): ?>
           <?php if (empty($content['field_project_image_mobile'])): ?>
-            <div class="field field-name-field-project-image field-type-image project-type-<?php print $variables['field_project_t_project_type__term']; ?> hide-for-small"><div class="field-items"><div class="field-item"><a href="<?php print image_style_url($variables['field_project_image__image_style__fullscreen'], $node->field_project_image['und'][0]['uri']); ?>" class="colorbox-inline" rel="node-field-project-image"><img typeof="foaf:Image" src="<?php print image_style_url($variables['field_project_image__image_style__node'], $node->field_project_image['und'][0]['uri']); ?>" alt="<?php print $node->field_project_image['und'][0]['alt']; ?>" title="<?php print $node->field_project_image['und'][0]['title']; ?>" /></a></div></div></div>
+            <div class="field field-name-field-project-image field-type-image project-type-<?php print $variables['field_project_t_project_type__term']; ?> hide-for-small"><div class="field-items"><div class="field-item"><a href="<?php print image_style_url($variables['field_project_image__image_style__fullscreen'], $node->field_project_image['und'][0]['uri']); ?>" class="colorbox" rel="node-field-project-image" title="<?php print $node->field_project_image['und'][0]['title']; ?>"><img typeof="foaf:Image" src="<?php print image_style_url($variables['field_project_image__image_style__node'], $node->field_project_image['und'][0]['uri']); ?>" alt="<?php print $node->field_project_image['und'][0]['alt']; ?>" title="<?php print $node->field_project_image['und'][0]['title']; ?>" /></a></div></div></div>
           <?php else: ?>
             <div class="field field-name-field-project-image field-type-image hide-for-small"><div class="field-items"><div class="field-item">
-              <a href="<?php print image_style_url($variables['field_project_image__image_style__fullscreen'], $node->field_project_image['und'][0]['uri']); ?>" class="colorbox-inline" rel="node-field-project-image">
+              <a href="<?php print image_style_url($variables['field_project_image__image_style__fullscreen'], $node->field_project_image['und'][0]['uri']); ?>" class="colorbox" rel="node-field-project-image">
                 <img typeof="foaf:Image" src="<?php print image_style_url($variables['field_project_image__image_style__node'], $node->field_project_image['und'][0]['uri']); ?>" alt="" />
                 <img class="field-name-field-project-image-mobile-laptop-overlay" typeof="foaf:Image" src="<?php print image_style_url('project_image_mobile_laptop_overlay', $node->field_project_image_mobile['und'][0]['uri']); ?>" alt="<?php print $node->field_project_image_mobile['und'][0]['alt']; ?>" title="<?php print $node->field_project_image_mobile['und'][0]['title']; ?>" />
               </a>
-              <a href="<?php print image_style_url('project_image_mobile_modal_fullscreen', $node->field_project_image_mobile['und'][0]['uri']); ?>" class="colorbox-inline" rel="node-field-project-image"></a>
             </div></div></div>
           <?php endif; ?>
         <?php else: ?>
@@ -137,12 +135,12 @@
         $node_field_project_image_2 = '';
         if (!empty($content['field_project_image_2'])) {
           foreach ($node->field_project_image_2['und'] as $key => $value) {
-            $node_field_project_image_2[] = '<div class="field field-name-field-project-image-2 field-type-image thumbnail ' . $foundation_grid_column_classes . '"><div class="field-items"><div class="field-item"><a href="' . image_style_url('project_image_modal_fullscreen', $node->field_project_image_2['und'][$key]['uri']) . '" class="colorbox-inline" rel="node-field-project-image"><img typeof="foaf:Image" src="' . image_style_url('project_image_2_node', $node->field_project_image_2['und'][$key]['uri']) . '" alt="' . $node->field_project_image_2['und'][$key]['alt'] . '" title="' . $node->field_project_image_2['und'][$key]['title'] . '" /></a></div></div></div>';
+            $node_field_project_image_2[] = '<div class="hide field field-name-field-project-image-2 field-type-image thumbnail ' . $foundation_grid_column_classes . '"><div class="field-items"><div class="field-item"><a href="' . image_style_url('project_image_modal_fullscreen', $node->field_project_image_2['und'][$key]['uri']) . '" class="colorbox" rel="node-field-project-image" title="' . $node->field_project_image_2['und'][$key]['title'] . '"><img typeof="foaf:Image" src="' . image_style_url('project_image_2_node', $node->field_project_image_2['und'][$key]['uri']) . '" alt="' . $node->field_project_image_2['und'][$key]['alt'] . '" title="' . $node->field_project_image_2['und'][$key]['title'] . '" /></a></div></div></div>';
           }
         }
         if (!empty($content['field_project_image_mobile'])) {
           foreach ($node->field_project_image_mobile['und'] as $key => $value) {
-            $node_field_project_image_2[] = '<div class="field field-name-field-project-image-mobile field-type-image thumbnail ' . $foundation_grid_column_classes . '"><div class="field-items"><div class="field-item"><a href="' . image_style_url('project_image_mobile_modal_fullscreen', $node->field_project_image_mobile['und'][$key]['uri']) . '" class="colorbox-inline" rel="node-field-project-image"><img typeof="foaf:Image" src="' . image_style_url('project_image_mobile_2_node', $node->field_project_image_mobile['und'][$key]['uri']) . '" alt="' . $node->field_project_image_mobile['und'][$key]['alt'] . '" title="' . $node->field_project_image_mobile['und'][$key]['title'] . '" /></a></div></div></div>';
+            $node_field_project_image_2[] = '<div class="hide field field-name-field-project-image-mobile field-type-image thumbnail ' . $foundation_grid_column_classes . '"><div class="field-items"><div class="field-item"><a href="' . image_style_url('project_image_mobile_modal_fullscreen', $node->field_project_image_mobile['und'][$key]['uri']) . '" class="colorbox" rel="node-field-project-image" title="' . $node->field_project_image_mobile['und'][$key]['title'] . '"><img typeof="foaf:Image" src="' . image_style_url('project_image_mobile_2_node', $node->field_project_image_mobile['und'][$key]['uri']) . '" alt="' . $node->field_project_image_mobile['und'][$key]['alt'] . '" title="' . $node->field_project_image_mobile['und'][$key]['title'] . '" /></a></div></div></div>';
           }
         }
         // Count the number of items in the new array.
